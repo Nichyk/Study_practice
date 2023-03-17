e4925d70799ed965d69dbb40b80d1548ddeadb73
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-03-17 10:30:09 +0200

    add solution for task 2

---
+++ Lesson 5/task_2.py
@@ -0,0 +1,7 @@
+import random
+# The birthday greeting program.
+# Write a program that takes your name as input, and then your age as input and greets you with the following:
+# “Hello <name>, on your next birthday you’ll be <age+1> years”
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+print(f'Hello {name}, on your next birthday you\'ll be {int(age) + 1} years')
