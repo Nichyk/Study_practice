1f0340836b76545e538aa8cee794ed97e3af5d4e
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-03-19 18:52:25 +0200

    add return and delete func call

--- Lesson 8/task_1.py
+++ Lesson 8/task_1.py
@@ -1,7 +1,6 @@
 # Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie.
 # The function should then print “My favorite movie is named {name}”.
 def favourite_movie(name):
-    print(f'My favorite movie is named {name}')
-
-
-favourite_movie('Avatar')
+    answer = f'My favorite movie is named {name}'
+    print(answer)
+    return answer
