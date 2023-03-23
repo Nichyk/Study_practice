ff512699bf621af05b4a19c65c1a712e5f8b999d
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-03-23 16:28:43 +0200

    update solution for task 2

---
+++ Lesson 9/task_2/module_to_change_path.py
@@ -0,0 +1,7 @@
+import sys
+
+print(sys.path)
+sys.path.insert(0, 'C:\\Users\\nichy\\Study\\BeetrootAcademy\\Study_practice\\Lesson 9\\task_2\\another_path')
+print(sys.path)
+sys.path.remove('C:\\Users\\nichy\\Study\\BeetrootAcademy\\Study_practice\\Lesson 9\\task_2\\another_path')
+print(sys.path)
