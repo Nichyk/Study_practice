6c736ab561e3306e9a388f3fccf185cfee0b16ba
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-03-25 16:05:28 +0200

    update solution for task_2

--- Lesson 9/task_2/module_to_change_path.py
+++ Lesson 9/task_2/module_to_change_path.py
@@ -6,12 +6,10 @@ try:
 except ModuleNotFoundError:
     print('You must add path to module before you import it')
 
-sys.path.insert(0, 'C:\\Users\\nichy\\Study\\BeetrootAcademy\\Study_practice\\Lesson 9\\task_2\\another_path')
+sys.path.append('C:\\Users\\nichy\\Study\\BeetrootAcademy\\Study_practice\\Lesson 9\\task_2\\another_path')
+print(sys.path)
 
 try:
     import summary_of_two
 except ModuleNotFoundError:
     print('You must add path to module before you import it')
-
-help(summary_of_two)
-sum_of_two(3, 4)
