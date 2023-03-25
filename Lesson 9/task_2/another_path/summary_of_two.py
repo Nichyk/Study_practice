d2e5535f80ee6980a3d1daba6edfdf2b800eac35
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-03-25 15:44:13 +0200

    update solution for task_2

---
+++ Lesson 9/task_2/another_path/summary_of_two.py
@@ -0,0 +1,7 @@
+print('Module was imported')
+
+
+def sum_of_two(a, b):
+    """Func to sum two values"""
+    result = a + b
+    return result
--- Lesson 9/task_2/module_to_change_path.py
+++ Lesson 9/task_2/module_to_change_path.py
@@ -1,7 +1,17 @@
 import sys
+print(sys.path)
 
-print(sys.path)
+try:
+    import summary_of_two
+except ModuleNotFoundError:
+    print('You must add path to module before you import it')
+
 sys.path.insert(0, 'C:\\Users\\nichy\\Study\\BeetrootAcademy\\Study_practice\\Lesson 9\\task_2\\another_path')
-print(sys.path)
-sys.path.remove('C:\\Users\\nichy\\Study\\BeetrootAcademy\\Study_practice\\Lesson 9\\task_2\\another_path')
-print(sys.path)
+
+try:
+    import summary_of_two
+except ModuleNotFoundError:
+    print('You must add path to module before you import it')
+
+help(summary_of_two)
+sum_of_two(3, 4)
