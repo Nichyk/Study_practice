c563e4e9713c42d232d4e40a59ba5de1dfd169c5
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-04-24 17:35:00 +0500

    add structure for task_1

---
+++ Lesson 17/task_1.py
@@ -0,0 +1,42 @@
+# Method overloading.
+# Create a base class named Animal with a method called talk and then create two subclasses: Dog and Cat,
+# and make their own implementation of the method talk be different.
+# For instance, Dog’s can be to print ‘woof woof’, while Cat’s can be to print ‘meow’.
+
+# Also, create a simple generic function, which takes as input instance of a Cat or Dog classes and performs
+# talk method on input parameter.
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def talk(self):
+        pass
+
+
+class Dog(Animal):
+    def talk(self):
+        text = 'woof woof'
+        return text
+
+
+class Cat(Animal):
+    def talk(self):
+        text = 'meow'
+        return text
+
+
+cat1 = Cat()
+dog1 = Dog()
+
+
+def animal_talk(*args):
+    if not isinstance(args, Animal):
+        raise ValueError('Not animal')
+    elif isinstance(args, Dog):
+        return args.talk()
+    elif isinstance(args, Cat):
+        return args.talk()
+
+
+print(animal_talk(dog1))
