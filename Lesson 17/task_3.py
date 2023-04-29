e510fcce61d88e804ce10e50a9b02168c0997e3f
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-04-29 10:08:25 +0500

    add full solution for task 3

--- Lesson 17/task_3.py
+++ Lesson 17/task_3.py
@@ -29,8 +29,26 @@ class Fraction:
         new_other_nom, new_other_denom = other.denom, other.nom
         return Fraction(nom=self.nom * new_other_nom, denom=self.denom * new_other_denom)
 
+    def __lt__(self, other):
+        return self.nom / self.denom < other.nom / other.denom
+
+    def __eq__(self, other):
+        return self.nom / self.denom == other.nom / other.denom
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        return not self.__lt__(other)
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
+
 
 if __name__ == "__main__":
     x = Fraction(1, 2)
     y = Fraction(1, 4)
-    x + y == Fraction(3, 4)
+    assert x + y == Fraction(3, 4)
