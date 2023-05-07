badacc08ccfea021c310c43014ed885c632004d0
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-05-07 13:36:49 +0500

    added a solution for task 1

---
+++ Lesson 19/task_1.py
@@ -0,0 +1,8 @@
+# Create your own implementation of a built-in function enumerate, named `with_index`, which takes two parameters:
+# `iterable` and `start`, default is 0. Tips: see the documentation for the enumerate function
+
+def with_index(iterable, start=0):
+    index = start
+    for value in iterable:
+        yield index, value
+        index += 1
