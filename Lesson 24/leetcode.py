5cb8c4c72aeb8100cab6fe941e9bcf68bf71df15
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-05-23 22:20:42 +0500

    added few fixes for leetcode task

--- Lesson 24/leetcode.py
+++ Lesson 24/leetcode.py
@@ -6,12 +6,11 @@ class Solution:
     def count_students(students: List[int], sandwiches: List[int]) -> int:
         count = 0
         while count < len(sandwiches):
-            if students[0] == sandwiches[-1]:
+            if students[0] == sandwiches[0]:
                 students.pop(0)
-                sandwiches.pop()
+                sandwiches.pop(0)
                 count = 0
             else:
-                student = students.pop(0)
-                students.append(student)
+                students.append(students.pop(0))
                 count += 1
         return len(students)
