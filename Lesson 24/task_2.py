3de9dc8616544e4098c0bb4be80a216907d39365
Serhii Nichyk <nichyk.sergey@gmail.com>
2023-05-28 16:16:32 +0500

    added another solution for task 2

--- Lesson 24/task_2.py
+++ Lesson 24/task_2.py
@@ -1,15 +1,37 @@
 # Write a program that reads in a sequence of characters, and determines whether it's parentheses, braces,
 # and curly brackets are "balanced."
+from collections import deque
+
 
 def balanced(sequence: str) -> bool:
-    if sequence.count('{') != sequence.count('}') or sequence.find('{') > sequence.find('}'):
-        print('\'{}\' not balanced')
+    stack = deque()
+    brackets = {')': '(', '}': '{', ']': '['}
+    if len(sequence) <= 1:
         return False
-    elif sequence.count('[') != sequence.count(']') or sequence.find('[') > sequence.find(']'):
-        print('\'[]\' not balanced')
-        return False
-    elif sequence.count('(') != sequence.count(')') or sequence.find('(') > sequence.find(')'):
-        print('\'()\' not balanced')
-        return False
-    else:
-        return True
+    for i in sequence:
+        if i in brackets.values():
+            stack.append(i)
+        if i in brackets.keys():
+            if len(stack) == 0:
+                return False
+            else:
+                stack.append(i)
+                if stack.count(i) != stack.count(brackets[i]):
+                    return False
+    return True
+
+
+if __name__ == '__main__':
+    print(balanced('()'))
+    print(balanced('()[]{}'))
+    print(balanced('(]'))
+
+
+
+
+
+
+
+
+
+
